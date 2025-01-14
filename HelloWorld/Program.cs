e0145200871619a5a5981a9aa4ab3800cd527cdc
FranciscoRibeiroSilva <franciscoribeirsil@gmail.com>
2025-01-14 15:41:50 -0300

    minha primeira vez mexendo no codigo

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string aBoaDoMonento;
+Console.WriteLine("Me diz a boa do momento.");
+aBoaDoMonento = Console.ReadLine();
+Console.WriteLine("Então esta me dizendo que "+aBoaDoMonento);
